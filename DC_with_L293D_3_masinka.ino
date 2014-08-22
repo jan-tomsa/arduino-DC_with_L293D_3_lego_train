1fe1be0de8922d6001b2d89334be6436d29464dd
jan-tomsa <jan.tomsa.1976@gmail.com>
2014-08-22 23:43:05 +0200

    Fixed LEDs indicating direction, better structure + comments

--- DC_with_L293D_3_masinka.ino
+++ DC_with_L293D_3_masinka.ino
@@ -13,6 +13,8 @@ int pin_led_green = 13;
 
 char direction1 = 'X';
 
+
+/////////////////////////// SETUP //////////////////////////////
 void setup() {                
   // initialize the motor digital pins as outputs.
   pinMode(pin_motor_enable, OUTPUT);     
@@ -24,8 +26,20 @@ void setup() {
   direction1 = 'X';
   pinMode(pin_led_red,   OUTPUT);     
   pinMode(pin_led_green, OUTPUT);
+  digitalWrite(pin_led_red,LOW);
+  digitalWrite(pin_led_green,LOW);
 }
 
+/////////////////////////// LEDs //////////////////////////////
+void led_red(int val) {
+  digitalWrite(pin_led_red,val);
+}
+
+void led_green(int val) {
+  digitalWrite(pin_led_green,val);
+}
+
+/////////////////////////// MOTOR //////////////////////////////
 void motor1( int val_1, int val_2, int val_3 ) {
   digitalWrite(pin_motor_enable, val_1);
   digitalWrite(pin_motor_in1, val_2);
@@ -54,14 +68,9 @@ void goBackwardTimed(int delayInMs) {
   delay(delayInMs);
 }
 
-void led_red(int val) {
-  digitalWrite(pin_led_red,val);
-}
-
-void led_green(int val) {
-  digitalWrite(pin_led_green,val);
-}
-
+///////////////////////////////////////////////////////////////////
+///////////////////////  LOOP  ////////////////////////////////////
+///////////////////////////////////////////////////////////////////
 // the loop routine runs over and over again forever:
 void loop() {
   if (direction1 == 'X') {
@@ -78,13 +87,13 @@ void loop() {
   dig_back = digitalRead(pin_in_back);
   if (dig_fwd  == LOW  &&  dig_back == HIGH)  {
     direction1 = 'F';
-    led_red(LOW);
-    led_green(HIGH);
+    led_red(HIGH);
+    led_green(LOW);
   }
   if (dig_fwd  == HIGH   &&  dig_back == LOW) {
     direction1 = 'B';
-    led_red(HIGH);
-    led_green(LOW);
+    led_red(LOW);
+    led_green(HIGH);
   }
   if (dig_fwd  == LOW  &&  dig_back == LOW) {
     direction1 = 'X';
